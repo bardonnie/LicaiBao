faf2943364e3de62d843bbf0728d967e99ee84fe
chenxin <chenxin8879@126.com>
2014-01-26 17:53:27 +0800

    Initial Commit

---
+++ LicaiBao/LC_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LC_AppDelegate.h
+//  LicaiBao
+//
+//  Created by mac on 14-1-26.
+//  Copyright (c) 2014年 trends-china. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LC_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LicaiBao/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LicaiBao
+//
+//  Created by mac on 14-1-26.
+//  Copyright (c) 2014年 trends-china. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LC_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LC_AppDelegate class]));
+    }
+}
